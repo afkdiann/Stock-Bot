c35c1ca3a4f026cdb996b4671d8e97d0cebe96c2
afkdiann <dianchengt123@gmail.com>
2026-08-17 17:25:35 -0400

    fix: changed os method used to retrieve environment variables

--- app/agent.py
+++ app/agent.py
@@ -4,7 +4,7 @@ from google.adk.agents.llm_agent import LlmAgent
 from .prompt import orchestrator_prompt
 
 root_agent = LlmAgent(
-    model=os.getenv("MODEL_NAME"),
+    model=os.environ.get("MODEL_NAME"),
     name='stock_analysis_orchestrator',
     description='Stock Analysis Orchestrator',
     instruction=orchestrator_prompt,
