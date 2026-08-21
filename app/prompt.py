4dbfabec901e62ab68089f7a0929656e096d62be
afkdiann <dianchengt123@gmail.com>
2026-08-20 22:30:53 -0400

    fix: improved orchestrator agent prompt

--- app/prompt.py
+++ app/prompt.py
@@ -1,3 +1,7 @@
-orchestrator_prompt = """
-You are a stock analysis agent
+ORCHESTRATOR_PROMPT = """
+You are a stock analysis agent.
+
+Your job is to analyze a stock based on a user query and provide a comprehensive report.
+
+To do this, you should use the tools you have access to to gather information about the stock and then use that information to create a report.
 """
